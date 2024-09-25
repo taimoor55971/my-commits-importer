e62c5e6db6be79b80750af7704ed203e9973665d
taimoor <taimur.ultracodes@gmail.com>
2024-09-25 09:56:11 +0000

    add code in files types: .ts,.tsx,.css remove code in files types: .ts,.tsx,.css

--- content.ts
+++ content.ts
@@ -1,4 +1,4 @@
 console.log("eblog")
 console.log("hjbya")
 console.log("hajup")
-console.log("kbwbg")
+console.log("mnabx")
--- content.tsx
+++ content.tsx
@@ -1,2 +1,2 @@
 console.log("rraxl")
-console.log("jceus")
+console.log("iwkcn")
